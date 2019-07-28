c3a0f38a38d24ccf08f1ab11d1ce04af54e918ba
OLAOSEBIKAN, Sulaiman Olabisi <Olabisisulaiman@gmail.com>
2019-07-28 10:18:10 +0100

    display of unique Influencer toasters on the page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import Button from './components/button';
 import Toast from './components/toast';
 import './css/index.css';
 import Header from './components/homeHeader';
@@ -8,11 +7,14 @@ class App extends Component {
   state = {  
     toastDisplay: false,
     influencers: getInfluencers(),
-    currentInfluencer: 0
+    currentInfluencer: 0,
+    influencersToDisplay: []
   };
 
   displayToast = val => {
+    const { influencers, currentInfluencer} = this.state;
     this.setState( { toastDisplay: true, currentInfluencer: this.getInfluencerToDisplay()});
+    this.populateInfluencers(influencers[currentInfluencer]);
   }
 
   getInfluencerToDisplay = () => {
@@ -20,9 +22,16 @@ class App extends Component {
     return Math.floor(Math.random() * inflencersCount);
   }
 
+  populateInfluencers = influencer => {
+    const influencers = [...this.state.influencersToDisplay];
+    const index = influencers.indexOf(influencer);
+    if (index <0) influencers.push(influencer);
+    this.setState( { influencersToDisplay: influencers });
+}
+
 
   render() { 
-    const { toastDisplay, influencers, currentInfluencer } = this.state;
+    const { toastDisplay, influencers, currentInfluencer, influencersToDisplay } = this.state;
     return ( 
       <div className = 'container-fluid'>
         <Header 
@@ -30,7 +39,7 @@ class App extends Component {
         />        
         <Toast 
           displayToast = { toastDisplay }
-          influencers = { influencers }
+          influencers = { influencersToDisplay }
           currentInfluencer = { currentInfluencer }
         />
       </div>
--- src/components/toast.jsx
+++ src/components/toast.jsx
@@ -3,33 +3,38 @@ import '../css/toast.css';
 import '../css/index.css';
 class Toast extends Component {
     state = {  
+        influencers: []
     }
+
     render() { 
         const { displayToast, influencers, currentInfluencer } = this.props;
         if (displayToast) 
         return (
-            <div className="row">
-                <div className="col-md-6 offset-md-6">
-                        <div className = 'cards' style={{ 'marginTop': '20px'}}>
-                            <div className= 'cardsBody' > 
-                                <div className="d-flex align-items-center border-bottom py-3">
-                                    {/* <img className="img-sm rounded-circle" src="../assets/images/face4.png" alt="" /> */}
-                                    <i className="mdi mdi-human-greeting mr-1"></i>
-                                        <div className="wrapper w-100 ml-3">
-                                            <p className="mb-0"><b>{ influencers[currentInfluencer].fullName } </b></p>
-                                            <div className="d-flex justify-content-between align-items-center">
-                                                <div className="d-flex align-items-center">
-                                                    <i className="mdi mdi-clock-outline text-muted mr-1"></i>
-                                                    <small className="text-muted ml-auto">2 hours ago</small>
+            <React.Fragment>
+                <div className="row">
+                  { influencers.map( influencer => 
+                        <div className="col-md-6">
+                            <div className = 'cards' style={{ 'marginTop': '20px'}}>
+                                <div className= 'cardsBody' >
+                                    <div className="list d-flex align-items-center border-bottom py-3">
+                                        <i className="mdi mdi-human-greeting mr-1"></i>
+                                            <div className="wrapper w-100 ml-3">
+                                                <p className="mb-0"><b></b> { influencer.fullName }</p>
+                                                <div className="d-flex justify-content-between align-items-center">
+                                                    <div className="d-flex align-items-center">
+                                                        <i className="mdi mdi-clock-outline text-muted mr-1"></i>
+                                                        <small className="text-muted ml-auto">2 hours ago</small>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-
-                </div>
-            </div>
+                       )}
+                    </div>
+             
+            </React.Fragment>
         )
         return null;
     }
